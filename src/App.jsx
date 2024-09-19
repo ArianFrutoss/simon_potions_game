8accf185d8fc39781a187a9b936fe152e860a3c9
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-09-19 23:12:24 +0200

    [ADD] - Add a part of the sound to each color

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, useRef } from 'react'
+import useSound from 'use-sound';
+import simon from './assets/sounds/sprite.mp3';
+import './App.css'
+
+function App() {
+  
+  const blueRef = useRef(null);
+  const yellowRef = useRef(null);
+  const greenRef = useRef(null);
+  const redRef = useRef(null);
+
+  const [play] = useSound(simon, {
+
+    sprite: {
+
+      one: [0, 500],
+      two: [1000, 500],
+      three: [2000, 500],
+      four: [3000, 500],
+      error: [4000, 1000],
+    },
+  });
+}
+
+export default App;
